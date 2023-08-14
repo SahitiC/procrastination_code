8529a76150ba280caea9e201d141c2d853563c9b
Sahiti Chebolu <schebolu@mpi.localnet>
2023-08-14 15:03:53 +0200

    testing tiger problem with pomdp_py
    Implements the tiger problem as recommened by pomdp_py to check that everything runs including the external solvers.

--- Tiger_problem.py
+++ Tiger_problem.py
@@ -1,25 +1,61 @@
+"""
+Re-creating the standard Tiger example from pomdp_py to test out the correct
+way of implementation. 
+"""
 import pomdp_py
+from pomdp_py.utils import TreeDebugger
 import random
+import numpy as np
+import sys
 
-class State(pomdp_py.State):
+class TigerState(pomdp_py.State):
     def __init__(self, name):
-        if name != "tiger-left" and name != "tiger-right":
-            raise ValueError("Invalid state: %s" % name)
         self.name = name
-        
-class Action(pomdp_py.Action):
+    def __hash__(self):
+        return hash(self.name)
+    def __eq__(self, other):
+        if isinstance(other, TigerState):
+            return self.name == other.name
+        return False
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return "TigerState(%s)" % self.name
+    def other(self):
+        if self.name.endswith("left"):
+            return TigerState("tiger-right")
+        else:
+            return TigerState("tiger-left")
+
+class TigerAction(pomdp_py.Action):
     def __init__(self, name):
-        if name != "open-left" and name != "open-right"\
-           and name != "listen":
-            raise ValueError("Invalid action: %s" % name)
         self.name = name
-        
-class Observation(pomdp_py.Observation):
+    def __hash__(self):
+        return hash(self.name)
+    def __eq__(self, other):
+        if isinstance(other, TigerAction):
+            return self.name == other.name
+        return False
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return "TigerAction(%s)" % self.name
+
+class TigerObservation(pomdp_py.Observation):
     def __init__(self, name):
-        if name != "tiger-left" and name != "tiger-right":
-            raise ValueError("Invalid action: %s" % name)
         self.name = name
-        
+    def __hash__(self):
+        return hash(self.name)
+    def __eq__(self, other):
+        if isinstance(other, TigerObservation):
+            return self.name == other.name
+        return False
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return "TigerAction(%s)" % self.name
+    
+# Observation model
 class ObservationModel(pomdp_py.ObservationModel):
     def __init__(self, noise=0.15):
         self.noise = noise
@@ -41,22 +77,22 @@ class ObservationModel(pomdp_py.ObservationModel):
             thresh = 0.5
 
         if random.uniform(0,1) < thresh:
-            return Observation(next_state.name)
+            return TigerObservation(next_state.name)
         else:
-            return Observation(next_state.other().name)
+            return TigerObservation(next_state.other().name)
 
     def get_all_observations(self):
         """Only need to implement this if you're using
-        a solver that needs to enumerate over the observation
-        space (e.g. value iteration)"""
-        return [Observation(s)
+        a solver that needs to enumerate over the observation space
+        (e.g. value iteration)"""
+        return [TigerObservation(s)
                 for s in {"tiger-left", "tiger-right"}]
-    
+
+# Transition Model
 class TransitionModel(pomdp_py.TransitionModel):
     def probability(self, next_state, state, action):
         """According to problem spec, the world resets once
-        action is open-left/open-right. Otherwise, it
-        stays the same"""
+        action is open-left/open-right. Otherwise, stays the same"""
         if action.name.startswith("open"):
             return 0.5
         else:
@@ -69,15 +105,14 @@ class TransitionModel(pomdp_py.TransitionModel):
         if action.name.startswith("open"):
             return random.choice(self.get_all_states())
         else:
-            return State(state.name)
+            return TigerState(state.name)
 
     def get_all_states(self):
         """Only need to implement this if you're using
-        a solver that needs to enumerate over the
-        observation space (e.g. value iteration)"""
-        return [State(s) for s in {"tiger-left", "tiger-right"}]
-    
-    
+        a solver that needs to enumerate over the observation space (e.g. value iteration)"""
+        return [TigerState(s) for s in {"tiger-left", "tiger-right"}]
+
+# Reward Model
 class RewardModel(pomdp_py.RewardModel):
     def _reward_func(self, state, action):
         if action.name == "open-left":
@@ -96,13 +131,37 @@ class RewardModel(pomdp_py.RewardModel):
     def sample(self, state, action, next_state):
         # deterministic
         return self._reward_func(state, action)
-    
-    
-class TigerProblem(pomdp_py.POMDP):
 
-    def __init__(self, obs_noise, init_true_state):
+# Policy Model
+class PolicyModel(pomdp_py.RolloutPolicy):
+    """A simple policy model with uniform prior over a
+       small, finite action space"""
+    ACTIONS = [TigerAction(s)
+              for s in {"open-left", "open-right", "listen"}]
+
+    def sample(self, state):
+        return random.sample(self.get_all_actions(), 1)[0]
+
+    def rollout(self, state, history=None):
+        """Treating this PolicyModel as a rollout policy"""
+        return self.sample(state)
+
+    def get_all_actions(self, state=None, history=None):
+        return PolicyModel.ACTIONS
+
+
+class TigerProblem(pomdp_py.POMDP):
+    """
+    In fact, creating a TigerProblem class is entirely optional
+    to simulate and solve POMDPs. But this is just an example
+    of how such a class can be created.
+    """
+
+    def __init__(self, obs_noise, init_true_state, init_belief):
         """init_belief is a Distribution."""
-        agent = pomdp_py.Agent(TransitionModel(),
+        agent = pomdp_py.Agent(init_belief,
+                               PolicyModel(),
+                               TransitionModel(),
                                ObservationModel(obs_noise),
                                RewardModel())
         env = pomdp_py.Environment(init_true_state,
